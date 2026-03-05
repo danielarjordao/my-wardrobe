7637ddf81a38cc632322df7070155a132e7d5473
danielarjordao <danielarjordao@hotmail.com>
2026-03-05 15:24:26 +0000

    feat: add dynamic lists for form dropdowns and UI filters

---
+++ src/app/models/item-options.ts
@@ -0,0 +1,12 @@
+// Dynamic lists for the form dropdowns and UI filters
+export const availableCategories: string[] =
+  	['Tops', 'Bottoms', 'Outerwear', 'One-Pieces', 'Shoes', 'Accessories'];
+
+export const availableStatuses: string[] =
+  	['Available', 'In Laundry', 'Packed', 'Repair', 'Other'];
+
+export const availableColors: string[] =
+	['Black', 'White', 'Blue', 'Red', 'Green', 'Yellow', 'Purple', 'Pink', 'Brown', 'Gray', 'Multicolor', 'Other'];
+
+export const  availableSortOptions: string[] =
+	['Newest', 'Oldest', 'Price: Low to High', 'Price: High to Low'];
