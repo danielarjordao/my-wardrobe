9e985c2f12bd6c24a10ab6216ffee8305083f464
danielarjordao <danielarjordao@hotmail.com>
2026-03-05 18:58:53 +0000

    feat: create Outfits component with HTML, CSS, and TypeScript for managing saved outfits

---
+++ src/app/pages/outfits-list/outfits-list.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { WardrobeService } from '../../services/wardrobe';
+import { ClothingItem } from '../../models/clothing-item';
+import { Outfit } from '../../models/outfit';
+import { OutfitService } from '../../services/outfit';
+import { OutfitCard } from '../../components/outfit-card/outfit-card';
+
+@Component({
+  selector: 'app-outfits-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, OutfitCard],
+  templateUrl: './outfits-list.html',
+  styleUrl: './outfits-list.css'
+})
+export class Outfits {
+  wardrobeItems: ClothingItem[] = [];
+  savedOutfits: Outfit[] = [];
+
+  constructor(
+    private wardrobeService: WardrobeService,
+    private outfitService: OutfitService
+  ) {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.wardrobeItems = this.wardrobeService.getAllItems();
+    this.savedOutfits = this.outfitService.getAllOutfits();
+  }
+
+  deleteOutfit(id: string): void {
+    if (confirm('Are you sure you want to delete this look?')) {
+      this.outfitService.deleteOutfit(id);
+      this.loadData();
+    }
+  }
+}
