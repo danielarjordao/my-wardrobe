683139e551de60153cef4d6604f22bf11ef3091e
danielarjordao <danielarjordao@hotmail.com>
2026-03-05 19:00:56 +0000

    fix: correct import path for Outfits component and clean up navbar by removing Add Item button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { WardrobeList } from './pages/wardrobe-list/wardrobe-list';
 import { ItemForm } from './pages/item-form/item-form';
-import { Outfits } from './pages/outfits/outfits';
+import { Outfits } from './pages/outfits-list/outfits-list';
+import { OutfitForm } from './pages/outfit-form/outfit-form';
 
 export const routes: Routes = [
   // Redirect empty path to dashboard
@@ -16,4 +17,8 @@ export const routes: Routes = [
   // Forms (Create and Edit use the same component)
   { path: 'clothes/new', component: ItemForm },
   { path: 'clothes/edit/:id', component: ItemForm },
+
+  // Form (Create and Edit for outfits - reusing the same component)
+  { path: 'outfits/new', component: OutfitForm },
+  { path: 'outfits/edit/:id', component: OutfitForm },
 ];
