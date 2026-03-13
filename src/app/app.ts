004fd7f27bf57426b3b0acda52d55d583cc48583
danielarjordao <danielarjordao@hotmail.com>
2026-03-13 13:58:45 +0000

    feat: set up Docker environment with Dockerfile, docker-compose, and mock API

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // Import the navbar component
 import { Navbar } from './resources/navbar/navbar';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,17 @@ import { Navbar } from './resources/navbar/navbar';
 })
 export class App {
   title = 'My Wardrobe';
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  async loadUsers() {
+    const response = await fetch(environment.apiUrl + '/users');
+    const users = await response.json();
+
+    console.log(users);
+  }
+
 }
+
