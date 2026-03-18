e188dddc872d26ae5981715fc15fdebd29350933
danielarjordao <danielarjordao@hotmail.com>
2026-03-18 10:09:37 +0000

    chore: remove development environment configuration files for security reasons

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  apiUrl: 'http://localhost:3001',
-  supabaseUrl: 'https://fdwbdxqsfesqsghtuiln.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkd2JkeHFzZmVzcXNnaHR1aWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NjEwNDYsImV4cCI6MjA4OTIzNzA0Nn0.Tdv6ZRBfkY7BSPaVIpGlhzWowlDhU_J-kFxaRHbW5Ws'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  apiUrl: 'http://localhost:3001',
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseKey: 'YOUR_SUPABASE_ANON_KEY'
-};
